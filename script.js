ad661d51cd80db3362ec987d15d9ee94d1485191
Raissa Barbosa <raissabbsa@gmail.com>
2022-08-20 11:59:49 -0300

    Versao dois - cartas embaralhadas e virando

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
 let qtdCartas;
+let organizaCartas = [];
+let defineImagens = [];
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 
 function IniciarJogo() {
 
@@ -19,13 +25,33 @@ function IniciarJogo() {
 
     const cartoes = document.querySelector('.cartoes');
 
+    for(i=0 ; i < qtdCartas/2 ; i++) {
+        for(let j = 0 ; j<2; j++)
+            organizaCartas.push(i);
+    }
+    organizaCartas.sort(comparador);
+    console.log(organizaCartas);
+
+    defineImagens.push(`<img src="imagens/bobrossparrot.gif" />`);
+    defineImagens.push(`<img src="imagens/explodyparrot.gif" />`);
+    defineImagens.push(`<img src="imagens/fiestaparrot.gif" />`);
+    defineImagens.push(`<img src="imagens/metalparrot.gif" />`);
+    defineImagens.push(`<img src="imagens/revertitparrot.gif" />`);
+    defineImagens.push(`<img src="imagens/tripletsparrot.gif" />`);
+    defineImagens.push(`<img src="imagens/unicornparrot.gif" />`);
 
     for(let i=0 ; i < qtdCartas ; i++) {
-        cartoes.innerHTML += `<div class="carta">
+        cartoes.innerHTML += `<div class="carta numero${organizaCartas[i]}" onclick="viraCarta(this)">
             <img src="imagens/front.png" />
+            ${defineImagens[organizaCartas[i]]}
         </div>`;
 
     }
 }
 
 IniciarJogo();
+
+function viraCarta(cartaVirada) {
+    cartaVirada.classList.add('selecionado');
+}
+
